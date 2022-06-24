2b67f2fb3c074c8ed3eedaaaad2f9de99a76c3da
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-24 11:17:41 -0500

    working on detials

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,19 +1,31 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 
 function MovieDetails(){
     const movies = useSelector(store => store.movies);
+    const  {id}  = useParams();
+
+    console.log('movie id is:', id);
 
     return(
-        <section className="movies">
-        {movies.map(movie => {
-            return (
-                <div key={movie.id} >
-                    <p>{movie.description}</p>
-                </div>
-            );
-        })}
-    </section>
+        <>
+            <h1>MovieDetails</h1>
+            <Link to="/"> HOME </Link>
+            <section className="movies">
+            {movies.map(movie => {
+                if (movie.id === id)
+                return (
+                    <div key={movie.id}>
+                        <p>{movie.description}</p>
+                    </div>
+                );
+                else (
+                    console.log('no matches dawg')
+                )
+            })}
+            </section>
+        </>
     )
 };
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { Link } from 'react-router-dom'; 
+import {useHistory} from 'react-router-dom'
+
+
 
 function MovieList() {
-
+    const history = useHistory()
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const handleClick = () => {
+        console.log("You are headed to movie details");
+        history.push('/details/:id');
+      };
 
     return (
         <main>
@@ -19,7 +28,7 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} onClick={handleClick}/>
                         </div>
                     );
                 })}

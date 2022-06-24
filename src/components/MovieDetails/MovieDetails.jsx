6fde1aa3a0518e67ed230d2da41c6f4da99734ef
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-24 14:11:18 -0500

    finished base funcitonality

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -4,22 +4,24 @@ import { Link, useParams } from 'react-router-dom';
 
 function MovieDetails(){
     const movies = useSelector(store => store.movies);
-    const  {id}  = useParams();
+    let  {id}  = useParams();
 
-    console.log('movie id is:', id);
+    for(let i = 0; i<movies.length; i++){
+        if(movies[i].id === Number(id)){
+            id = i
+            break
+        }
+    }
 
     return(
         <>
             <h1>MovieDetails</h1>
             <Link to="/"> HOME </Link>
+
             <section className="movies">
-            {movies.map(movie => {
-                return (
-                    <div key={movie.id}>
-                        <p>{movie.description}</p>
-                    </div>
-                );
-            })}
+                        <div key={movies[id].id}>
+                            <p>{movies[id].description}</p>
+                        </div>
             </section>
         </>
     )
